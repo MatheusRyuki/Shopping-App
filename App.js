ed5c2206f4dab68b1d7913a9eb7eebe6cbc80f94
Matheus <dr_4_gao@hotmail.com>
2019-12-09 10:48:34 -0300

    Preparando o Navigator Principal

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Text, View } from "react-native";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import ShopNavigator from "./navigation/ShopNavigation";
 
 import productsReducer from "./store/reducers/products";
 
@@ -14,9 +14,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
-        <Text>A new app!</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
