dda741c1f50b12b645e86e9f3e1090dce985b188
Matheus <dr_4_gao@hotmail.com>
2019-12-21 19:34:11 -0300

    Atualizando e Deletando produtos do servidor

--- store/actions/products.js
+++ store/actions/products.js
@@ -43,9 +43,17 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-  return {
-    type: DELETE_PRODUCT,
-    pid: productId
+  return async dispatch => {
+    await fetch(
+      `https://react-hooks-7a9a2.firebaseio.com/products/${productId}.json`,
+      {
+        method: "DELETE"
+      }
+    );
+    dispatch({
+      type: DELETE_PRODUCT,
+      pid: productId
+    });
   };
 };
 
@@ -83,13 +91,30 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl
-    }
+  return async dispatch => {
+    await fetch(
+      `https://react-hooks-7a9a2.firebaseio.com/products/${id}.json`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl
+        })
+      }
+    );
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl
+      }
+    });
   };
 };
